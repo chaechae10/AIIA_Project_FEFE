e7b5fff3a269f98331328470686517f9141827dc
김채연 <gimchaeyeon57@gmail.com>
2024-09-23 23:57:47 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/aiia/FEFE/FefeApplication.java
@@ -0,0 +1,13 @@
+package com.aiia.FEFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FefeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FefeApplication.class, args);
+	}
+
+}
